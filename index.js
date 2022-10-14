46239877d2c275e8ea8e261408f966ae1eef7fe5
Katerina Florou <95655231+KatFlorou@users.noreply.github.com>
2022-10-14 10:46:38 +0200

    nav/services/contact changes

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-// ------------------- Navbar ----------------------------
+document.addEventListener('DOMContentLoaded', () => {
 
-// In smaller screens hide the navbar links and show only brand name and menu icon
-// Get the icon thw opens the menu
-let iconMenu = document.getElementsByClassName("icon_menu")[0]
-// Get the menu list
-let navbarList = document.getElementsByClassName("navbar_list")[0]
-// When user clicks on menu icon show menu list
-iconMenu.addEventListener("click", () => {
-  navbarList.classList.toggle("active")
-})
-// When the user clicks a link within the menu list, close the menu list
-navbarList.addEventListener("click", () => {
-  navbarList.classList.toggle("active")  
-})
+  // ------------------- Navbar ----------------------------
+  // In smaller screens hide the navbar links and show only brand name and menu icon
+  // Get the icon that opens the menu
+  const iconMenu = document.querySelector(".icon_menu")
+  // Get the dropdown menu
+  const smallScreenMenu = document.querySelector(".smallScreenMenu")
+  // Get the menu list
+  const navbarList = document.querySelector(".navbar_list")
+  
+  iconMenu.addEventListener("click", () => {
+    //Copy menu list to new dropdown menu
+    smallScreenMenu.innerHTML = navbarList.innerHTML 
+    // When user clicks on menu icon show menu list
+    smallScreenMenu.classList.toggle("active")
+  })
+  
+  // When the user clicks a link within the menu list, close the menu list
+  smallScreenMenu.addEventListener("click", () => {
+    smallScreenMenu.classList.toggle("active")  
+  })
 
-// Change cursor to pointer when touching menu icon
-let menu = document.getElementsByClassName("icon_menu")[0]
-menu.addEventListener("touchstart", () =>  {
-  menu.style.cursor = "pointer";
-})
 
-
-// ------------------- Modal Careers ---------------------
-// Get the modal
-var modal = document.getElementById("careers_modal");
-// Get the button that opens the modal
-var btn = document.getElementById("jobs");
-// Get the <span> element that closes the modal
-var span = document.getElementById("close");
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // ------------------- Modal Careers ---------------------
+  // Get the modal
+  const modal = document.getElementById("careers_modal");
+  // Get the button that opens the modal
+  const btn = document.getElementById("jobs");
+  // Get the <span> element that closes the modal
+  const span = document.getElementById("close");
+  // When the user clicks on the button, open the modal
+  btn.onclick = () => {
+    modal.style.display = "block";
+  }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = () => {
     modal.style.display = "none";
   }
-}
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+})
+
